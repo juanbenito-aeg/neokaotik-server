b2ab42f7f277505cb01258477cb2df6002b1c420
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-11-23 21:38:01 +0100

    feat(NK-173): add live_positions model specifying the collection

---
+++ src/models/live-positionModel.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, model } from "mongoose";
+import { pointSchema } from "../schemas/generics";
+import ILivePosition from "../interfaces/ILivePosition";
+
+const livePositionSchema = new Schema<ILivePosition>(
+  {
+    player: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Player",
+      required: true,
+    },
+    location: { type: pointSchema, required: true },
+  },
+  {
+    collection: "live_positions",
+  }
+);
+
+const LivePositions = model<ILivePosition>("LivePosition", livePositionSchema);
+
+export default LivePositions;

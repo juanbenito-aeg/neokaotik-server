8dcce1e46942b3a85d00d0e82c007a9b079e8a5d
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-11-23 21:03:52 +0100

    feat(NK-169): create "updateArtifactsByField" function in DB layer

--- src/database/artifactDatabase.ts
+++ src/database/artifactDatabase.ts
@@ -1,8 +1,16 @@
 import Artifact from "../models/artifactModel";
+import { Fields } from "../interfaces/generics";
 
 async function getArtifacts() {
   const artifacts = await Artifact.find({});
   return artifacts;
 }
 
-export default { getArtifacts };
+async function updateArtifactsByField(
+  fieldToFilterBy: Fields,
+  changesToApply: Fields
+) {
+  await Artifact.updateMany(fieldToFilterBy, changesToApply);
+}
+
+export default { getArtifacts, updateArtifactsByField };

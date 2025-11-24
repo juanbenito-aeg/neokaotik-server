9077651cf3530c1a2cd1b685d64daf01e2fd60c7
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-11-24 11:58:38 +0100

    fix: remove live position related code & update artifact model

--- src/models/artifactModel.ts
+++ src/models/artifactModel.ts
@@ -1,7 +1,16 @@
 import { Schema, model } from "mongoose";
 import IArtifact from "../interfaces/IArtifact";
 import { ArtifactState } from "../constants";
-import { pointSchema } from "../schemas/generics";
+
+const pointSchema = new Schema(
+  {
+    type: { type: String, enum: ["Point"], required: true },
+    coordinates: { type: [Number], required: true },
+  },
+  {
+    _id: false,
+  }
+);
 
 const artifactSchema = new Schema<IArtifact>({
   name: { type: String, required: true },

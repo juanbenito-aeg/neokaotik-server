bba9dc467d7bc7904401f42a933e41d0deea7ef3
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-12-11 18:38:53 +0100

    refactor: remove unnecessary "mqtt" package mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,13 +1,6 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-jest.mock("mqtt", () => ({
-  __esModule: true,
-  default: {
-    connect: () => ({ on: () => {} }),
-  },
-}));
-
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGODB_URI_TEST!);
 });

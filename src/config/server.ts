9b843b73ac489265996352f1525a9d6ef1e48642
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-12-12 09:18:10 +0100

    refactor(NK-243): create "missions" router & use it in "server.ts"

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { createServer } from "node:http";
 import authRoutes from "../routes/auth.routes";
 import playerRoutes from "../routes/player.routes";
-import artifactRoutes from "../routes/artifactRoutes";
+import missionRoutes from "../routes/mission.routes";
 
 const app = express();
 
@@ -11,6 +11,6 @@ const httpServer = createServer(app);
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/players", playerRoutes);
-app.use("/api/artifacts", artifactRoutes);
+app.use("/missions", missionRoutes);
 
 export { app, httpServer };
--- src/routes/artifactRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import artifactControllers from "../controllers/mission.controllers";
-
-const router = express.Router();
-
-router.get("/", artifactControllers.getArtifacts);
-
-export default router;

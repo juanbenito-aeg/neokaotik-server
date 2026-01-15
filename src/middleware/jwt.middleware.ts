efc53a9bb2cca2a9536192bf6be85af512ca09b2
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2026-01-15 09:25:26 +0100

    feat(NK-328): create "verifyJWT" middleware

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { jwtServices } from "../services/jwt.services";
+import * as jwt from "jsonwebtoken";
 
 function createJWT(req: Request, res: Response, next: NextFunction) {
   const playerEmail = res.locals.playerEmail;
@@ -10,6 +11,38 @@ function createJWT(req: Request, res: Response, next: NextFunction) {
   next();
 }
 
-const jwtMiddleware = { createJWT };
+function verifyJWT(req: Request, res: Response, next: NextFunction) {
+  const authenticationHeader = req.headers.authorization;
+
+  const accessOrRefreshToken = authenticationHeader?.split(" ")[1];
+
+  if (!accessOrRefreshToken) {
+    console.log("No access/refresh token was provided.");
+    return res.sendStatus(401);
+  }
+
+  const secretKey =
+    req.path === "/refresh"
+      ? process.env.REFRESH_TOKEN_SECRET!
+      : process.env.ACCESS_TOKEN_SECRET!;
+
+  try {
+    const { data: playerEmail } = jwt.verify(
+      accessOrRefreshToken,
+      secretKey
+    ) as jwt.JwtPayload;
+
+    console.log("Access/Refresh token valid.");
+
+    res.locals.playerEmail = playerEmail;
+
+    next();
+  } catch (error) {
+    console.log("Access token is expired.");
+    res.status(403).send("Access token is expired.");
+  }
+}
+
+const jwtMiddleware = { createJWT, verifyJWT };
 
 export { jwtMiddleware };

d2745ec6d846cc4429ebbeafa8a6e66f8f6c41b0
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2026-01-16 12:14:03 +0100

    refactor: set amount of time between each cron task execution to original value

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -1,6 +1,6 @@
 import cron from "node-cron";
 import { weakenNonBetrayerAcolytes } from "../tasks/weaken-non-betrayer-acolytes";
 
-const CRON_EXPRESSION = "*/2 * * * *"; // Change it to "*/15 * * * * *" to run the task every 15 seconds for testing purposes
+const CRON_EXPRESSION = "*/30 * * * *"; // Change it to "*/15 * * * * *" to run the task every 15 seconds for testing purposes
 
 cron.schedule(CRON_EXPRESSION, weakenNonBetrayerAcolytes);
